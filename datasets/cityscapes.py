f24bc66b064d2301bb72e96d64f4e23125ba0efa
Federico Fortunati <s282788@studenti.polito.it>
2024-05-06 20:24:57 +0200

    Fixed dataset class for Cityscapes

--- datasets/cityscapes.py
+++ datasets/cityscapes.py
@@ -1,21 +1,84 @@
 from torch.utils.data import Dataset
-
+import os
+import posixpath
+import numpy as np
 # TODO: implement here your custom dataset class for Cityscapes
-
-
+from torchvision.io import read_image 
+def denormalize(image):
+    image = image.to('cpu').numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    image = image * std + mean
+    image = np.clip(image, 0, 1)
+    return image
 class CityScapes(Dataset):
-    def __init__(self):
+    def __init__(self,annotations_dir,images_dir,transform=None, target_transform=None):
         super(CityScapes, self).__init__()
-        # TODO
+        self.images_dir = images_dir
+        self.annotations_dir = annotations_dir
+        self.transform = transform
+        self.target_transform = target_transform
+        self.map_index_to_image = {}
+        self.map_index_to_annotation = {}
+        
+        counter=self.generate_map_images(path=self.images_dir)
+        #print(counter)
+        counter=self.generate_map_annotations(path=self.annotations_dir)
+        #print(len(self.map_index_to_annotation))
+        #print(counter)
 
-        pass
+    """def list_files_recursive(self,path='.',counter=0):
+        for entry in os.listdir(path):
+            full_path = posixpath.join(path, entry)
+            if os.path.isdir(full_path):
+                counter=counter+self.list_files_recursive(full_path)
+            else:
+                counter = counter+1
+        return counter"""
+    def generate_map_images(self,path='.',counter=0):
+        
+        for entry in os.listdir(path):
+            full_path = posixpath.join(path, entry)
+            if os.path.isdir(full_path):
+                counter=self.generate_map_images(full_path,counter)
+            else:
+
+                self.map_index_to_image[counter]=full_path
+                counter = counter+1
+
+        return counter
+    def generate_map_annotations(self,path='.',counter=0):
+        for entry in os.listdir(path):
+            full_path = posixpath.join(path, entry)
+            if os.path.isdir(full_path):
+                counter=self.generate_map_annotations(full_path,counter)
+            elif full_path.endswith("color.png"):
+                self.map_index_to_annotation[counter]=full_path
+                counter = counter+1
+        return counter
 
     def __getitem__(self, idx):
-        # TODO
-
-        pass
+        image=""
+        annotation=""
+        image_path = self.map_index_to_image[idx]
+        annotation_path = self.map_index_to_annotation[idx]
+        image = read_image(image_path).permute(1, 2, 0)
+        annotation = read_image(annotation_path).permute(1,2,0)
+        return image, annotation
 
     def __len__(self):
-        # TODO
+        return len(self.map_index_to_annotation)
+ 
 
-        pass
+    # def __getitem__(self, idx):
+    #     img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+    #     image = read_image(img_path)
+    #     label = self.img_labels.iloc[idx, 1]
+    #     if self.transform:
+    #         image = self.transform(image)
+    #     if self.target_transform:
+    #         label = self.target_transform(label)
+    #     return image, label
+    
+    #step 1: map each index to an image
+    #step 2: map each index to a label
--- train.py
+++ train.py
@@ -1,1 +1,23 @@
 # TODO: Define here your training and validation loops.
+from datasets.cityscapes import CityScapes
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+dataset_path='C:/Users/Federico/Desktop/Universita/1_Magistrale/2_semestre/MLDL/proj/MLDL_project/datasets/Cityscapes/Cityscapes/Cityspaces/'
+annotation_train=dataset_path+'gtFine/train'
+image_train=dataset_path+'images/train'
+
+annotation_val=dataset_path+'gtFine/val'
+image_val=dataset_path+'images/val'
+
+cityscapes_train = CityScapes(annotations_dir=annotation_train, images_dir=image_train)
+cityscapes_val = CityScapes(annotations_dir=annotation_val, images_dir=image_val)
+print(cityscapes_train.__len__())
+print(cityscapes_val.__len__())
+print(cityscapes_train.map_index_to_image[5])
+print(cityscapes_train.map_index_to_annotation[1571])
+#cityscapes_train.list_files_recursive(path=image_train)
+image,annotation=cityscapes_train.__getitem__(5)
+fig, axes = plt.subplots(2, 1, figsize=(1, 10))
+axes[0].imshow(image)
+axes[1].imshow(annotation)
+plt.show()
