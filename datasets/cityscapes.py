eac7033f62b79bfab4ea0e48457c14cac106c384
Federico Fortunati <s282788@studenti.polito.it>
2024-05-24 00:41:09 +0200

    Added gta2city_dacs_kaggle runned for 10 epochs

--- datasets/cityscapes.py
+++ datasets/cityscapes.py
@@ -2,8 +2,13 @@ from torch.utils.data import Dataset
 import os
 import posixpath
 import numpy as np
+import torch
 # TODO: implement here your custom dataset class for Cityscapes
 from torchvision.io import read_image 
+from torchvision import transforms
+from torchvision.transforms import v2
+from torchvision import tv_tensors
+import torchvision.transforms.functional as F
 def denormalize(image):
     image = image.to('cpu').numpy().transpose((1, 2, 0))
     mean = np.array([0.485, 0.456, 0.406])
@@ -11,74 +16,77 @@ def denormalize(image):
     image = image * std + mean
     image = np.clip(image, 0, 1)
     return image
+#custom transformation for padding. Given a target and a fill, it applies a costant padding.
+#Useful when image is cropped and it's necessary to feed an images with constant size.
+class SquarePad:
+  def __call__(self, image,target,fill=0):
+    s = image.shape
+    h=target[-2]
+    w=target[-1]
+    hp = int((h - s[-2])/2)
+    vp = int((w - s[-1])/2)
+    padding = (vp, hp, vp, hp)
+    return F.pad(image, padding, fill, 'constant')
 class CityScapes(Dataset):
-    def __init__(self,annotations_dir,images_dir,transform=None, target_transform=None):
+    def __init__(self,annotations_dir,images_dir,transform=None, target_transform=None,applier=None):
         super(CityScapes, self).__init__()
         self.images_dir = images_dir
         self.annotations_dir = annotations_dir
         self.transform = transform
         self.target_transform = target_transform
-        self.map_index_to_image = {}
-        self.map_index_to_annotation = {}
-        
-        counter=self.generate_map_images(path=self.images_dir)
-        #print(counter)
-        counter=self.generate_map_annotations(path=self.annotations_dir)
-        #print(len(self.map_index_to_annotation))
-        #print(counter)
-
-    """def list_files_recursive(self,path='.',counter=0):
+        self.map_index_to_image = []
+        self.map_index_to_annotation = []
+        self.generate_map_images(path=self.images_dir)
+        self.generate_map_annotations(path=self.annotations_dir)
+        self.pad_transformation = SquarePad()
+        # set of transformation that are applied during training for data augmentation purposes
+        self.applier = applier
+    def generate_map_images(self,path='.'):
         for entry in os.listdir(path):
             full_path = posixpath.join(path, entry)
             if os.path.isdir(full_path):
-                counter=counter+self.list_files_recursive(full_path)
+                self.generate_map_images(full_path)
             else:
-                counter = counter+1
-        return counter"""
-    def generate_map_images(self,path='.',counter=0):
-        
+                #self.map_index_to_image[counter]=full_path
+                self.map_index_to_image.append(full_path)
+    def generate_map_annotations(self,path='.'):
         for entry in os.listdir(path):
             full_path = posixpath.join(path, entry)
             if os.path.isdir(full_path):
-                counter=self.generate_map_images(full_path,counter)
-            else:
-
-                self.map_index_to_image[counter]=full_path
-                counter = counter+1
-
-        return counter
-    def generate_map_annotations(self,path='.',counter=0):
-        for entry in os.listdir(path):
-            full_path = posixpath.join(path, entry)
-            if os.path.isdir(full_path):
-                counter=self.generate_map_annotations(full_path,counter)
+                self.generate_map_annotations(full_path)
+            #else:
             elif full_path.endswith("Ids.png"):
-                self.map_index_to_annotation[counter]=full_path
-                counter = counter+1
-        return counter
+                self.map_index_to_annotation.append(full_path)
 
     def __getitem__(self, idx):
         image=""
         annotation=""
         image_path = self.map_index_to_image[idx]
-        annotation_path = self.map_index_to_annotation[idx]
-        image = read_image(image_path).permute(1, 2, 0)
-        annotation = read_image(annotation_path).permute(1,2,0)
+        image = read_image(image_path)
+        #return last two elements of the path
+        path=image_path.split('/')
+        image_name = posixpath.join(path[-2],path[-1])
+        
+        annotation_path = posixpath.join(self.annotations_dir, image_name.replace("_leftImg8bit.png","_gtFine_labelTrainIds.png"))
+        #annotation_path = self.map_index_to_annotation[idx]
+        annotation = read_image(annotation_path)#[0:3,:,:]
+        #convert annotation to mask tv_tensor; In this way, transforms are able to properly transform the label.
+        annotation_tv=tv_tensors.Mask(annotation)
+        #image_t,annotation_t=applier2(image,annotation_tv)
+        if self.applier:
+            image_shape=image.shape
+            annotation_shape=annotation.shape
+            image,annotation=self.applier(image,annotation_tv)
+            #check if cropping has been executed. In that case, apply padding 
+            if image.shape != image_shape:
+                image=self.pad_transformation(image,image_shape)
+                annotation=self.pad_transformation(annotation,annotation_shape,fill=255)
+            #v2.RandomApply(transforms=[v2.RandomCrop(size=(256, 256))], p=1)
+        if self.transform:
+             image = self.transform(image)
+             annotation= torch.tensor(self.transform(annotation),dtype=torch.uint8)
         return image, annotation
 
     def __len__(self):
-        return len(self.map_index_to_annotation)
+        return len(self.map_index_to_image)
  
-
-    # def __getitem__(self, idx):
-    #     img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-    #     image = read_image(img_path)
-    #     label = self.img_labels.iloc[idx, 1]
-    #     if self.transform:
-    #         image = self.transform(image)
-    #     if self.target_transform:
-    #         label = self.target_transform(label)
-    #     return image, label
-    
-    #step 1: map each index to an image
-    #step 2: map each index to a label
--- train.py
+++ train.py
@@ -1,7 +1,10 @@
 # TODO: Define here your training and validation loops.
+"""
 from datasets.cityscapes import CityScapes
 from torch.utils.data import DataLoader
+from models.deeplabv2.deeplabv2 import * 
 import matplotlib.pyplot as plt
+from torchvision import transforms
 dataset_path='C:/Users/Federico/Desktop/Universita/1_Magistrale/2_semestre/MLDL/proj/MLDL_project/datasets/Cityscapes/Cityscapes/Cityspaces/'
 annotation_train=dataset_path+'gtFine/train'
 image_train=dataset_path+'images/train'
@@ -9,15 +12,186 @@ image_train=dataset_path+'images/train'
 annotation_val=dataset_path+'gtFine/val'
 image_val=dataset_path+'images/val'
 
-cityscapes_train = CityScapes(annotations_dir=annotation_train, images_dir=image_train)
-cityscapes_val = CityScapes(annotations_dir=annotation_val, images_dir=image_val)
+cityscapes_train = CityScapes(annotations_dir=annotation_train, images_dir=image_train,transform=transforms.Resize(size = (512,1024)))
+cityscapes_val = CityScapes(annotations_dir=annotation_val, images_dir=image_val,transform=transforms.Resize(size = (512,1024)))
 print(cityscapes_train.__len__())
 print(cityscapes_val.__len__())
-print(cityscapes_train.map_index_to_image[5])
-print(cityscapes_train.map_index_to_annotation[5])
+index=55
+print(cityscapes_train.map_index_to_image[index])
+print(cityscapes_train.map_index_to_annotation[index])
 #cityscapes_train.list_files_recursive(path=image_train)
-image,annotation=cityscapes_train.__getitem__(5)
+image,annotation=cityscapes_train.__getitem__(index)
 fig, axes = plt.subplots(2, 1, figsize=(1, 10))
 axes[0].imshow(image)
 axes[1].imshow(annotation,cmap='gray')
 plt.show()
+from PIL import Image
+import numpy as np
+
+
+
+import torch.optim as optim
+
+# Set up data loaders
+train_loader = DataLoader(cityscapes_train, batch_size=4, shuffle=True, num_workers=4)
+val_loader = DataLoader(cityscapes_val, batch_size=4, shuffle=False, num_workers=4)
+
+# Initialize the model
+model = get_deeplab_v2(num_classes=19, pretrain=True, pretrain_model_path='C:/Users/Federico/Desktop/Universita/1_Magistrale/2_semestre/MLDL/proj/MLDL_project/models/deeplab_resnet_pretrained_imagenet.pth')
+model.to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+
+# Define loss function and optimizer
+criterion = nn.CrossEntropyLoss()  # Adjust if your model outputs different dimensions
+#optimizer = optim.Adam(model.optim_parameters(lr=0.0001))
+
+# Modify here to ensure all parameters are captured correctly
+def optim_parameters(model, lr):
+    return [
+        {'params': [p for p in model.get_1x_lr_params_no_scale()], 'lr': lr},
+        {'params': [p for p in model.layer6.parameters()], 'lr': 10 * lr}  # Directly accessing parameters of layer6
+    ]
+
+optimizer = optim.Adam(optim_parameters(model, 0.0001))
+
+# Training loop
+def train(num_epochs, model, loaders, criterion, optimizer, device=torch.device("cuda" if torch.cuda.is_available() else "cpu")):
+    model.train()
+    for epoch in range(num_epochs):
+        total_loss = 0
+        print(epoch)
+        for data in loaders['train']:
+            images, labels = data
+            images, labels = images.to(device), labels.to(device)
+            print("ciao")
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            
+            total_loss += loss.item()
+        
+        print(f"Epoch {epoch+1}, Loss: {total_loss / len(loaders['train'])}")
+
+# Validation loop
+def validate(model, loader, criterion, device):
+    model.eval()
+    total_loss = 0
+    with torch.no_grad():
+        for data in loader:
+            images, labels = data
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            total_loss += loss.item()
+            
+    print(f"Validation Loss: {total_loss / len(loader)}")
+
+# Running the training and validation
+loaders = {
+    'train': train_loader,
+    'validate': val_loader
+}
+train(1, model, loaders, criterion, optimizer)  # Train for 10 epochs as an example
+validate(model, val_loader, criterion)
+"""
+import torch
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import transforms
+import matplotlib.pyplot as plt
+from datasets.cityscapes import CityScapes
+from models.deeplabv2.deeplabv2 import get_deeplab_v2
+import numpy as np
+from PIL import Image
+import torch.nn as nn
+
+def optim_parameters(model, lr):
+    return [
+        {'params': [p for p in model.get_1x_lr_params_no_scale()], 'lr': lr},
+        {'params': [p for p in model.layer6.parameters()], 'lr': 10 * lr}
+    ]
+
+def train(num_epochs, model, loaders, criterion, optimizer, device):
+    model.train()
+    for epoch in range(num_epochs):
+        total_loss = 0
+        print(epoch)
+        for batch_idx, (inputs, targets) in enumerate(loaders['train']):
+            #print(data)
+            inputs, targets = inputs.to(device), targets.to(device)
+            print("Image shape:", inputs.shape)  # Ensure the shape is correct
+            print("Label shape:", targets.shape)  # Check the shape of labels if needed
+            inputs = inputs.float()
+            targets = targets.squeeze()
+            print("c8ao")
+            outputs = model(inputs)
+            optimizer.zero_grad()
+            print("ciao")
+            loss = criterion(outputs, targets)
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
+        print(f"Epoch {epoch+1}, Loss: {total_loss / len(loaders['train'])}")
+
+def validate(model, loader, criterion, device):
+    model.eval()
+    total_loss = 0
+    with torch.no_grad():
+        for data in loader:
+            images, labels = data
+            images, labels = images.to(device), labels.to(device)
+
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            total_loss += loss.item()
+    print(f"Validation Loss: {total_loss / len(loader)}")
+
+def main():
+    dataset_path = 'C:/Users/Federico/Desktop/Universita/1_Magistrale/2_semestre/MLDL/proj/MLDL_project/datasets/Cityscapes/Cityscapes/Cityspaces/'
+    annotation_train = dataset_path + 'gtFine/train'
+    image_train = dataset_path + 'images/train'
+    annotation_val = dataset_path + 'gtFine/val'
+    image_val = dataset_path + 'images/val'
+    resize_transform = transforms.Resize(interpolation=transforms.InterpolationMode.NEAREST_EXACT,size = (512,1024))
+    # Setup device agnostic code
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    cityscapes_train = CityScapes(annotations_dir=annotation_train, images_dir=image_train,transform=resize_transform)
+    cityscapes_val = CityScapes(annotations_dir=annotation_val, images_dir=image_val,transform=resize_transform)
+    """
+    print(cityscapes_train.__len__())
+    print(cityscapes_val.__len__())
+    index=55
+    print(cityscapes_train.map_index_to_image[index])
+    print(cityscapes_train.map_index_to_annotation[index])
+    #cityscapes_train.list_files_recursive(path=image_train)
+    image,annotation=cityscapes_train.__getitem__(index)
+    """
+    fig, axes = plt.subplots(2, 1, figsize=(1, 10))
+    axes[0].imshow(image)
+    axes[1].imshow(annotation,cmap='gray')
+    plt.show()
+"""
+    
+
+    train_loader = DataLoader(cityscapes_train, batch_size=16, shuffle=True)
+    val_loader = DataLoader(cityscapes_val, batch_size=16, shuffle=True)
+
+    model = get_deeplab_v2(num_classes=19, pretrain=True, pretrain_model_path='C:/Users/Federico/Desktop/Universita/1_Magistrale/2_semestre/MLDL/proj/MLDL_project/models/deeplab_resnet_pretrained_imagenet.pth')
+    model.to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(optim_parameters(model, 0.0001))
+
+    loaders = {
+        'train': train_loader,
+        'validate': val_loader
+    }
+
+    #train(1, model, loaders, criterion, optimizer, torch.device("cuda" if torch.cuda.is_available() else "cpu"))  # Train for 1 epoch as an example
+    #validate(model, val_loader, criterion, torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+"""
+
+
+if __name__ == '__main__':
+    main()
